9f6b71b97bbb6af21ab3e54aedc00d3dba58bfa1
unknown <tanbiirsakiib7@gmail.com>
2021-10-06 20:43:03 +0600

    done

--- src/App.js
+++ src/App.js
@@ -2,22 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const players = [
+    {name: "Messi", position: "AMF", club: "PSG"},
+    {name: "Ronaldo", position: "CF", club: "Real Madrid"},
+    {name: "Haaland", position: "CF", club: "Dortmund"},
+    {name: "Neymar", position: "LWF", club: "Barcelona"}
+
+  ]
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {players.map(player => <Player name = {player.name} position= {player.position} club= {player.club}></Player>)}
+    </div>
+  );
+}
+
+function Player(props) {
+  console.log(props)
+  const messiStyle = {
+    color: 'white',
+    backgroundColor: 'hotpink',
+    border: '2px solid blue',
+    margin: "10px",
+    borderRadius: "10px"
+  }
+  return (
+    <div style={messiStyle}>
+      <h2>{props.name}</h2>
+      <h3>Club: {props.club}</h3>
+      <h3>Position: {props.position}</h3>
     </div>
   );
 }
